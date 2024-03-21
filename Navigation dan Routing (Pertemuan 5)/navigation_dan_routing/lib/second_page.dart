9dd264eb5b81f348277456800efa29c2bcf3a521
daniya-y <daniyaa.y27@gmail.com>
2024-03-21 11:06:36 +0700

    Praktikum 5 Navigation dan Routing

---
+++ Navigation dan Routing (Pertemuan 5)/navigation_dan_routing/lib/second_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  final String? data;
+  const SecondPage({Key? key, this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final data2 = ModalRoute.of(context)!.settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              data ?? '',
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            data2 == null
+                ? const SizedBox()
+                : Text(
+                    data2.toString(),
+                    style: const TextStyle(fontSize: 20.0),
+                  ),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Kembali'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
