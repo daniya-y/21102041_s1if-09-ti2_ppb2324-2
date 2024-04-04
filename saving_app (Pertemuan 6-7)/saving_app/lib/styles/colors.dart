5c457c129713ace09ce6ba05d0fe4394e32685c9
daniya-y <daniyaa.y27@gmail.com>
2024-04-05 01:44:08 +0700

    Praktikum 6 dan 7 Saving App

---
+++ saving_app (Pertemuan 6-7)/saving_app/lib/styles/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const Color kBgColor = Color.fromARGB(255, 255, 173, 250);
+const Color kBlueRibbon = Color.fromARGB(158, 244, 76, 233);
+const Color kNightBlack = Color(0xFF2C2C2C);
+const Color kBlack = Color(0xFF000000);
+const Color kMatterhornBlack = Color(0xFF525252);
+const Color kNobelGray = Color(0xFF9F9F9F);
+const Color kLightGray = Color(0xFFCBCBCB);
+const Color kSuvaGray = Color(0xFF868686);
+const Color kGrey = Color(0xFF888888);
+const Color kDarkGray = Color(0xFFFABABAB);
+const Color kWhite = Color(0xFFFFFFFF);
+const Color kEgyptianBlue = Color.fromARGB(255, 169, 12, 154);
+const Color kLuckyBlue = Color(0xFF363853);
+const Color kTropicalBlue = Color.fromARGB(255, 255, 173, 254);
+const Color kTreeGreen = Color(0xFF6ED097);
+const Color kOrange = Color(0xFFF65725);
