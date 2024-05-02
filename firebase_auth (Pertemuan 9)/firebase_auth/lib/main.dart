541241d9c93a77c1c1de6007e7bf3ff6431979d6
daniya-y <daniyaa.y27@gmail.com>
2024-05-02 12:32:39 +0700

    Praktikum 9 Firebase Auth

---
+++ firebase_auth (Pertemuan 9)/firebase_auth/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth09/bloc/login/login_cubit.dart';
+import 'package:firebase_auth09/ui/splash.dart';
+import 'package:firebase_auth09/utils/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LoginCubit())
+      ],
+      child:
+MaterialApp(
+      title: "Praktikum 6",
+      debugShowCheckedModeBanner: false,
+      navigatorKey: NAV_KEY,
+      onGenerateRoute: generateRoute,
+      home: SplashScreen(),
+    ));
+  }
+}
